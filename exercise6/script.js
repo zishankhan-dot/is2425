138b628508c773c7ec6acc5068a1f3b14ec1e621
zishankhan-dot <56768232+zishankhan-dot@users.noreply.github.com>
2025-02-20 01:24:02 +0530

    Update script.js

--- exercise6/script.js
+++ exercise6/script.js
@@ -3,25 +3,14 @@ function fun(){
     let b=document.getElementById("L2").value.split(",").map(Number);
     alert(sol(a,b));
 }
-/*function sol(a,b){
+
+function sol(a,b){
     let sum=0;
     for (let i of b){
-        for(let j of a){
-            if(j%i===0){
-                sum+=j;
+        for (let j of a){
+            if(i%j===0){
+                sum+=i;
                 break;
-            }
-        }
-    }
-    return sum;
-}*/
-function sol(a, b) {
-    let sum = 0;
-    for (let j of b) {  // Loop through b
-        for (let i of a) {  // Loop through a
-            if (j % i === 0) {  
-                sum += j;  // Add j to sum
-                break;  // Break after first match (to avoid duplicate addition)
             }
         }
     }
